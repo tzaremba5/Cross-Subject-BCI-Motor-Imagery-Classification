3a5bcb7a5009b7af17fa2a80e457d0f9f6fe4786
Ted Zaremba <teddyzaremba@Teds-Mac-mini.local>
2022-02-06 13:35:34 -0600

    data

--- src/Data/load_data.py
+++ src/Data/load_data.py
@@ -9,23 +9,27 @@
 
 # Imports
 import argparse
+import os
 
 # load_BCI_Competition_IV_Dataset_IIa
 #
+# Loads in the data, prepreprocesses, and saves it to the outpath
+# Input: IV_IIa_path, IV_IIa_path
 #
-def load_BCI_Competition_IV_Dataset_IIa(IV_IIa_path):
+def load_BCI_Competition_IV_Dataset_IIa(IV_IIa_path, out_path):
 	print(IV_IIa_path)
+	print(out_path)
 
 # load_BCI_Competition_IV_Dataset_I
 #
 #
-def load_BCI_Competition_IV_Dataset_I(IV_I_path):
+def load_BCI_Competition_IV_Dataset_I(IV_I_path, out_path):
 	print(IV_I_path)
 
 # load_BCI_Competition_IV_Dataset_IIa(
 #
 #
-def load_BCI_Competition_IV_Dataset_IIa(III_IVa_path):
+def load_BCI_Competition_III_Dataset_IVa(III_IVa_path, out_path):
 	print(III_IVa_path)
 
 if __name__ == '__main__':
@@ -40,10 +44,17 @@ if __name__ == '__main__':
 	 help='BCI Competition III Dataset IVa raw path or $ to skip')
 	args = parser.parse_args()
 
+	# Sets the out_path to store all the preprocessed data
+	cwd = os.getcwd()
+	out_path = os.path.join(cwd, '{competition}')
+
 	# Loads all the competition data, stores the preprocessed data
 	if args.IV_IIa_path[0] != '$':
-		load_BCI_Competition_IV_Dataset_IIa(args.IV_IIa_path[0])
+		load_BCI_Competition_IV_Dataset_IIa(args.IV_IIa_path[0], 
+			out_path.format(competition = "IV_IIa"))
 	if args.IV_I_path[0] != '$':	
-		load_BCI_Competition_IV_Dataset_I(args.IV_I_path[0])
+		load_BCI_Competition_IV_Dataset_I(args.IV_I_path[0],
+			out_path.format(competition = "IV_I"))
 	if args.III_IVa_path[0] != '$':
-		load_BCI_Competition_IV_Dataset_IIa(args.III_IVa_path[0])
+		load_BCI_Competition_III_Dataset_IVa(args.III_IVa_path[0],
+			out_path.format(competition = "III_IVa"))
