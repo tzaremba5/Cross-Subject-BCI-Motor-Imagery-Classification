ddabdd8bdca3496b775431eaa5a5603b532911c4
Ted Zaremba <teddyzaremba@Teds-Mac-mini.local>
2022-02-11 00:16:58 -0600

    load data

--- src/Data/dataset_descriptions.py
+++ src/Data/dataset_descriptions.py
@@ -10,8 +10,8 @@ competitions = {
 	'IV_IIa': {
 		'subjects':['A01', 'A02', 'A03', 'A04'. 'A05', 'A06', 'A07', 'A08', 'A09'],
 		'left': ['A01', 'A02', 'A03', 'A04'. 'A05', 'A06', 'A07', 'A08', 'A09'],
-		'right': ['A01', 'A02', 'A03', 'A04'. 'A05', 'A06', 'A07', 'A08', 'A09']
-		'foot': ['A01', 'A02', 'A03', 'A05', 'A06', 'A07', 'A08', 'A09']
+		'right': ['A01', 'A02', 'A03', 'A04'. 'A05', 'A06', 'A07', 'A08', 'A09'],
+		'foot': ['A01', 'A02', 'A03', 'A05', 'A06', 'A07', 'A08', 'A09'],
 		'A01':{'left': 7, 'right': 8, 'foot': 9}, 
 		'A02':{'left': 7, 'right': 8, 'foot': 9}, 
 		'A03':{'left': 7, 'right': 8, 'foot': 9},
@@ -24,9 +24,9 @@ competitions = {
 	},
 	'IV_I': {
 		'subjects':['A', 'B', 'C', 'D', 'E', 'F', 'G'],
-		'left':['A', 'B', 'C', 'D', 'E', 'F', 'G']
-		'right':['B', 'C', 'D', 'E', 'G']
-		'foot': ['A', 'F'] 
+		'left':['A', 'B', 'C', 'D', 'E', 'F', 'G'],
+		'right':['B', 'C', 'D', 'E', 'G'],
+		'foot': ['A', 'F'], 
 		'A':{'left':1, 'foot':2},
 		'B':{'left':1, 'right':2}, 
 		'C':{'left':1, 'right':2}, 
@@ -36,12 +36,12 @@ competitions = {
 		'G':{'left':1, 'right':2}
 	},
 	'III_IVa': {
-		'subjects':['aa', 'al', 'av', 'aw', 'ay']
-		'left':[]
-		'right':['aa', 'al', 'av', 'aw', 'ay']
-		'foot':['aa', 'al', 'av', 'aw', 'ay']
+		'subjects':['aa', 'al', 'av', 'aw', 'ay'],
+		'left':[],
+		'right':['aa', 'al', 'av', 'aw', 'ay'],
+		'foot':['aa', 'al', 'av', 'aw', 'ay'],
 		'aa':{'right': 1, 'foot': 2}, 
-		'al':{'right': 1, 'foot': 2}, 
+		'al':{'right': 1, 'foot': 2},
 		'av':{'right': 1, 'foot': 2}, 
 		'aw':{'right': 1, 'foot': 2}, 
         'ay':{'right': 1, 'foot': 2}
