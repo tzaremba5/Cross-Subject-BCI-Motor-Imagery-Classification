1374bf01e599ab80fa6a4d3e3138ed2f7bde95da
Ted Zaremba <teddyzaremba@Teds-Mac-mini.local>
2022-02-12 15:54:02 -0600

    build features

--- src/Data/build_features.py
+++ src/Data/build_features.py
@@ -1,11 +1,24 @@
 ################################################################################
 # build_features.py
 #
-# Loads in the epochs generated from 
+# Loads in the epochs generated from the epochs_path
 #
+# WARNING: this file will save the STFT features
+#
+
+# Imports
+import os
+import pickle
+import numpy as np
+import scipy
+import mne
 
 # STFT_stacked
 #
+# Takes the epochs and generates all the STFT representations
+#
+# Input: MNE Epochs array
+# Output: List of numpy arrays containing the features
 #
 def STFT_stacked(epochs):
 	num_samples = epochs.shape[0]
@@ -22,17 +35,55 @@ def STFT_stacked(epochs):
 
 	return samples
 
-def create_features(competition_path, out_path):
+# load_competition
+#
+# Loads all the epochs for a gvien competition, generates features, and saves
+# them
+#
+# Input: Competition
+# Output: None
+#
+def load_competition(competition):
+	for subject in competitions[competition]['subjects']:
+		epochs_path = os.path.join(cwd, 'epochs', f'{competition}', f'{subject}.pk1')
 
+		twelve_channels = ['C1','C2','C3','C4','C5','C6','CP3','CP4',
+		'Cz','FC3','FC4','Fz']
+
+		infile = open(epochs_path, 'rb')
+		epochs = pickle.load(infile)
+
+		epochs = epochs.pick_channels(twelve_channels, ordered = True)
+
+		out_path = os.path.join(cwd, 'features', competition, subject)
+
+		# Makes the outpath to store the features
+		try:
+			os.makedirs(out_path)
+		except:
+			pass
 
 
 if __name__ == '__main__':
 	epochs_path = os.path.join(cwd, 'epochs', '{}')
 
 	cwd = os.getcwd()
-	out_path = cwd
 
 	for competition in ['IV_IIa', 'IV_I', 'III_IVa']:
-		build
 
-	out_path = os.path.join(cwd, '{}')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
