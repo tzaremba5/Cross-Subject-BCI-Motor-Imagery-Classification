a13c77fd8f84116637f8b9874431690a950edaff
Ted Zaremba <teddyzaremba@Teds-Mac-mini.local>
2022-02-12 22:43:37 -0600

    build-features

--- src/Data/build_features.py
+++ src/Data/build_features.py
@@ -15,15 +15,17 @@ import mne
 
 # STFT_stacked
 #
-# Takes the epochs and generates all the STFT representations
+# Takes the epochs and generates STFT representations
 #
 # Input: MNE Epochs array
 # Output: List of numpy arrays containing the features
 #
 def STFT_stacked(epochs):
+
 	num_samples = epochs.shape[0]
 	num_channels = epochs.shape[1]
 
+	# Iterates through, generating a STFT spectogram for every 1 epoch
 	samples = []
 	for e in range(num_samples):
 		stacked = []
@@ -37,39 +39,58 @@ def STFT_stacked(epochs):
 
 # load_competition
 #
-# Loads all the epochs for a gvien competition, generates features, and saves
-# them
+# For all competitions, subjects it Loads all the corresponding epochs,
+# generates features, and saves them to out_path_task
 #
 # Input: Competition
 # Output: None
 #
 def load_competition(competition):
-	for subject in competitions[competition]['subjects']:
-		epochs_path = os.path.join(cwd, 'epochs', f'{competition}', f'{subject}.pk1')
-
-		twelve_channels = ['C1','C2','C3','C4','C5','C6','CP3','CP4',
-		'Cz','FC3','FC4','Fz']
-
-		infile = open(epochs_path, 'rb')
-		epochs = pickle.load(infile)
-
-		epochs = epochs.pick_channels(twelve_channels, ordered = True)
-
-		out_path = os.path.join(cwd, 'features', competition, subject)
-
-		# Makes the outpath to store the features
-		try:
-			os.makedirs(out_path)
-		except:
-			pass
-
-
-if __name__ == '__main__':
-	epochs_path = os.path.join(cwd, 'epochs', '{}')
 
 	cwd = os.getcwd()
 
+	for subject in competitions[competition]['subjects']:
+
+		# Makes the directory to store the features
+		out_path_dir = os.path.join(cwd, 'features', competition, subject)
+		try:
+			os.makedirs(out_path_dir)
+		except:
+			pass
+
+		# Loads in the epochs
+		epochs_path = os.path.join(cwd, 'epochs', f'{competition}', f'{subject}.pk1')
+		infile = open(epochs_path, 'rb')
+		epochs = pickle.load(infile)
+
+		# Picks the 12 channels for MI classification
+		twelve_channels = ['C1','C2','C3','C4','C5','C6','CP3','CP4',
+		'Cz','FC3','FC4','Fz']
+		epochs = epochs.pick_channels(twelve_channels, ordered = True)
+
+		# Generates the STFT representations
+		for task in epochs.event_id.keys():
+			epochs_task = epochs[task].get_data()
+			STFT = STFT_Stacked(epochs_task)
+
+			out_path_task = os.path.join(out_path_dir, f'{task}.pk1')
+
+			# Removes all representation data from previous runs
+			try:
+				os.remove(out_path_task)
+			except:
+				pass
+
+			# Saves them
+			STFT_file = open(out_path_task, 'wb')
+			pickle.dump(STFT, STFT_file)
+
+
+
+if __name__ == '__main__':
+
 	for competition in ['IV_IIa', 'IV_I', 'III_IVa']:
+		load_competition(competition)
 
 
 
