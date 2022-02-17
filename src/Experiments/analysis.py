3b62844b8d9d8be826699114fac6bae69b2793b8
Teddy Zaremba <tzaremba5@gmail.com>
2022-02-16 21:18:55 -0600

    analysis

--- src/Experiments/analysis.py
+++ src/Experiments/analysis.py
@@ -3,3 +3,17 @@
 # analysis.py
 #
 
+# analysis
+#
+#
+#
+class analysis:
+
+	def __init__(self, subject, predictions, labels):
+		self.subject = subject
+		self.predictions = predictions
+		self.labels = labels
+
+	def analysis(self):
+		pass
+
--- src/Experiments/single-trial.py
+++ src/Experiments/single-trial.py
@@ -33,25 +33,25 @@ def single_trial(X, Y, num_classes):
 	sss = StratifiedShuffleSplit(n_splits=5, random_state=0)
   	es = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=50)
 
-  	# TODO GET PREDICTION INDECIES
-  	 for iteration in range(3):
-    sss.get_n_splits(X, Y)
-    for train_index, test_index in sss.split(X, Y): # gets the train and test indecies
-      model.load_weights('./checkpoints/my_checkpoint')
+  	for iteration in range(3):
+  		sss.get_n_splits(X, Y)
 
-      train_index.astype(int, copy = False)
-      test_index.astype(int, copy = False)
+	    for train_index, test_index in sss.split(X, Y): # gets the train and test indecies
+	      model.load_weights('./checkpoints/my_checkpoint')
 
-      X_train, X_test = X[train_index], X[test_index]
-      y_train, y_test = Y[train_index], Y[test_index]
+	      train_index.astype(int, copy = False)
+	      test_index.astype(int, copy = False)
 
-      model.fit(X_train, y_train, validation_data = (X_test, y_test), epochs = 1000, batch_size = 50, callbacks = [es], verbose = 0)
-      fold_predictions = model.predict(X_test)
+	      X_train, X_test = X[train_index], X[test_index]
+	      y_train, y_test = Y[train_index], Y[test_index]
 
-      # saves the predictions
-      indices += X_test.tolist()
-      predictions += fold_predictions.tolist()
-      labels += y_test.tolist()
+	      model.fit(X_train, y_train, validation_data = (X_test, y_test), epochs = 1000, batch_size = 50, callbacks = [es], verbose = 0)
+	      fold_predictions = model.predict(X_test)
+
+	      # saves the predictions
+	      indices += X_test.tolist()
+	      predictions += fold_predictions.tolist()
+	      labels += y_test.tolist()
 
   results = {'indices':indices, 'predictions':predictions, 'labels':labels}
   return results
@@ -64,4 +64,4 @@ if __name__ == '__main__':
 		for subject in competitions[competition]['subjects']:
 			X, Y, num_classes = create_dataset(competition, subject)
 			single_tria(X, Y, num_classes)
-	
+			
